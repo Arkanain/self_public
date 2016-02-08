d48f194d0e94453aa3857b3741ce9f6315804733
Arkanain <arkanainKiller@gmail.com>
2016-02-08 23:46:11 +0200

    add tests for sessions endpoint and fix article endpoint little bit

--- spec/api/articles_endpoint_spec.rb
+++ spec/api/articles_endpoint_spec.rb
@@ -15,32 +15,32 @@ describe Api::Endpoint::Articles, type: :rack do
     it 'should return list of articles' do
       2.times { create(:article, user: admin) }
 
-      get '/api/articles'
+      get 'api/articles'
 
       expect(response.successful?).to be_truthy
       expect(body.length).to eq(2)
     end
 
     it 'should not create new article' do
-      post '/api/articles', new_article_params
+      post 'api/articles', new_article_params
       expect(response.client_error?).to be_truthy
     end
 
     it 'should get single article' do
-      get "/api/articles/#{admin_article.id}"
+      get "api/articles/#{admin_article.id}"
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(admin_article.title)
     end
 
     it 'should not update single article' do
-      put "/api/articles/#{admin_article.id}", new_article_params
+      put "api/articles/#{admin_article.id}", new_article_params
 
       expect(response.client_error?).to be_truthy
     end
 
     it 'should not delete single article' do
-      delete "/api/articles/#{admin_article.id}"
+      delete "api/articles/#{admin_article.id}"
 
       expect(response.client_error?).to be_truthy
     end
@@ -54,35 +54,35 @@ describe Api::Endpoint::Articles, type: :rack do
     it 'should get list of articles' do
       2.times { create(:article, user: admin) }
 
-      get '/api/articles'
+      get 'api/articles'
 
       expect(response.successful?).to be_truthy
       expect(body.length).to eq(2)
     end
 
     it 'should get single article' do
-      get "/api/articles/#{admin_article.id}"
+      get "api/articles/#{admin_article.id}"
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(admin_article.title)
     end
 
     it 'should create new article' do
-      post '/api/articles', new_article_params
+      post 'api/articles', new_article_params
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(new_article_params[:title])
     end
 
     it 'should update single article' do
-      put "/api/articles/#{admin_article.id}", new_article_params
+      put "api/articles/#{admin_article.id}", new_article_params
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(new_article_params[:title])
     end
 
     it 'should delete single article' do
-      delete "/api/articles/#{admin_article.id}"
+      delete "api/articles/#{admin_article.id}"
 
       expect(response.successful?).to be_truthy
     end
@@ -96,47 +96,47 @@ describe Api::Endpoint::Articles, type: :rack do
     it 'should get list of articles' do
       2.times { create(:article, user: admin) }
 
-      get '/api/articles'
+      get 'api/articles'
 
       expect(response.successful?).to be_truthy
       expect(body.length).to eq(2)
     end
 
     it 'should get single article' do
-      get "/api/articles/#{admin_article.id}"
+      get "api/articles/#{admin_article.id}"
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(admin_article.title)
     end
 
     it 'should create single article' do
-      post '/api/articles', new_article_params
+      post 'api/articles', new_article_params
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(new_article_params[:title])
     end
 
     it 'should update his own article' do
-      put "/api/articles/#{writer_article.id}", new_article_params
+      put "api/articles/#{writer_article.id}", new_article_params
 
       expect(response.successful?).to be_truthy
       expect(body['title']).to eq(new_article_params[:title])
     end
 
     it 'should not update not his own article' do
-      put "/api/articles/#{admin_article.id}", new_article_params
+      put "api/articles/#{admin_article.id}", new_article_params
 
       expect(response.server_error?).to be_truthy
     end
 
     it 'should delete his own article' do
-      delete "/api/articles/#{writer_article.id}"
+      delete "api/articles/#{writer_article.id}"
 
       expect(response.successful?).to be_truthy
     end
 
     it 'should not delete not his own article' do
-      delete "/api/articles/#{admin_article.id}"
+      delete "api/articles/#{admin_article.id}"
 
       expect(response.server_error?).to be_truthy
     end
--- spec/api/sessions_endpoint_spec.rb
+++ spec/api/sessions_endpoint_spec.rb
@@ -7,11 +7,28 @@ describe Api::Endpoint::Sessions, type: :rack do
 
   let(:admin) { create(:admin) }
 
-  context 'as admin' do
-    it 'should return valid auth_token for admin' do
-      post '/api/sessions', { email: admin.email, password: admin.password }
+  it 'should return valid auth_token for admin' do
+    post 'api/sessions', { email: admin.email, password: admin.password }
 
-      expect(body['auth_token']).to eq(admin.authentication_token)
-    end
+    expect(response.successful?).to be_truthy
+    expect(body['auth_token']).to eq(admin.authentication_token)
+  end
+
+  it 'should not return auth_token for user with empty email or password' do
+    post 'api/sessions', { email: '', password: '' }
+
+    expect(response.client_error?).to be_truthy
+  end
+
+  it 'should not return auth_token with invalid email' do
+    post 'api/sessions', { email: 'admin_test@email', password: admin.password }
+
+    expect(response.client_error?).to be_truthy
+  end
+
+  it 'should sign_out user via auth token' do
+    delete 'api/sessions', { auth_token: admin.authentication_token }
+
+    expect(response.successful?).to be_truthy
   end
 end
